ce31671911710a36fe1998851794c83b800a53a1
DanielRestrepo53 <daniel.restrepo53@eia.edu.co>
2022-05-05 14:01:36 -0500

    Creacion parte del problema de jarras

---
+++ Proyecto 3 EDA/src/problemaJarras/Jarra.java
@@ -0,0 +1,27 @@
+package problemaJarras;
+
+public class Jarra {
+	
+	private int size;
+	private int cantAgua;
+	
+	public Jarra(int size) {
+		this.size = size;
+		cantAgua = 0;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+
+	public int getCantAgua() {
+		return cantAgua;
+	}
+
+	public void setCantAgua(int cantAgua) {
+		this.cantAgua = cantAgua;
+	}
+	
+	
+
+}
