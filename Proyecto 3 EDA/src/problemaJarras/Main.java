89c9169cef4e9eb9d22fc2e58a77a3a819bde1f9
DanielRestrepo53 <daniel.restrepo53@eia.edu.co>
2022-05-10 14:32:34 -0500

    Revision 2 Sol Problema

--- Proyecto 3 EDA/src/problemaJarras/Main.java
+++ Proyecto 3 EDA/src/problemaJarras/Main.java
@@ -5,9 +5,8 @@ import arbolAPS.*;
 public class Main {
 
 	public static void main(String[] args) {
-		APS<Jarras> arbol = new APS<Jarras>();
-		SolProblema solucion = new SolProblema(arbol);
-		
+		APS<Jarras> arbol = new APS<Jarras>(new Jarras(4,3,0,0));
+		SolProblema solucion = new SolProblema(arbol);	
 	}
 
 }
